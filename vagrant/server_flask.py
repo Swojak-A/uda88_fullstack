775a4c089845e625badb25e7bc796954dda7a443
swojak.a <swojak.a@gmail.com>
2018-05-30 16:43:11 +0200

    add new menu items, edit and delete routes

--- vagrant/server_flask.py
+++ vagrant/server_flask.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from _CRUD import  CRUD_Operator
+from _CRUD import CRUD_Operator
 
 
 app = Flask(__name__)
@@ -10,7 +10,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def mainPage():
-
+    restaurants = CRUD_Operator.get_all_restaurants()
+    print(restaurants)
     output = "<html><body>test</body></html>"
 
     return output
@@ -18,11 +19,25 @@ def mainPage():
 
 @app.route('/restaurant/<int:restaurant_id>')
 def restaurantMenu(restaurant_id):
-
     restaurant = CRUD_Operator.get_restaurant(restaurant_id)
     output = "<html><body>restaurant test: {}</body></html>".format(restaurant)
 
     return output
+
+
+@app.route('/restaurant/new_item')
+def newMenuItem(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
+
+
+@app.route('/restaurant/<int:restaurant_id>/item/<int:item_id>/edit')
+def editMenuItem(restaurant_id, menu_id):
+    return "page to edit a menu item. Task 2 complete!"
+
+
+@app.route('/restaurant/<int:restaurant_id>/item/<int:item_id>/delete')
+def deleteMenuItem(restaurant_id, menu_id):
+    return "page to delete a menu item. Task 3 complete!"
 
 
 
