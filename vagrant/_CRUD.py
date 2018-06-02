8b8fc9daa9984dc80135be41bb731d76e1ad4636
swojak.a <swojak.a@gmail.com>
2018-06-02 18:25:27 +0200

    add add_menu_item

--- vagrant/_CRUD.py
+++ vagrant/_CRUD.py
@@ -4,7 +4,7 @@ from db_database_setup import Base, Restaurant, MenuItem
 
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
-DBSession = sessionmaker(bind = engine)
+DBSession = (sessionmaker(bind = engine))
 session = DBSession()
 
 
@@ -12,7 +12,7 @@ class CRUD_Operator():
 
     @classmethod
     def get_restaurant(cls, id):
-        restaurant = session.query(Restaurant).filter_by(id=id).first()
+        restaurant = session.query(Restaurant).filter_by(id=id).one()
         return restaurant
 
     @classmethod
@@ -25,6 +25,26 @@ class CRUD_Operator():
         menu_items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id).all()
         return menu_items
 
+    @classmethod
+    def add_menu_item(cls, name, description, price, course, restaurant_id, add=False):
+        newItem = MenuItem(
+            name = name,
+            description = description,
+            price = price,
+            course = course,
+            restaurant_id=restaurant_id)
+        print(newItem.name, newItem.description, newItem.price, newItem.course, newItem.restaurant_id)
+        if add == True:
+            session.add(newItem)
+            session.commit()
+        else:
+            pass
+
+
+    @classmethod
+    def delete_menu_item(cls, item_id):
+        item_to_be_deleted = session.query(MenuItem).filter_by(id=item_id).one()
+        print("i could totally delete item {}: {}".format(item_to_be_deleted.id, item_to_be_deleted.name))
 
 
 
