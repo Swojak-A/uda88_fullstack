50434a540677e532e083ab8e5daf2030845d71bd
swojak.a <swojak.a@gmail.com>
2018-06-02 19:29:21 +0200

    switch to flask_sqlalchemy

--- vagrant/server_flask.py
+++ vagrant/server_flask.py
@@ -1,18 +1,33 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
-from _CRUD import CRUD_Operator, get_all_rest
-
-
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///restaurantmenu.db'
+db = SQLAlchemy(app)
 
 
+class Restaurant(db.Model):
+    __tablename__ = 'restaurant'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+
+class MenuItem(db.Model):
+    __tablename__ = 'menu_item'
+
+    name = db.Column(db.String(80), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(250))
+    price = db.Column(db.String(8))
+    course = db.Column(db.String(250))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'))
+    restaurant = db.relationship(Restaurant)
 
 
 @app.route('/')
 def mainPage():
-    restaurants = CRUD_Operator.get_all_restaurants()
-    print(restaurants)
+    restaurants = Restaurant.query.all()
+    # print([i.name for i in restaurants])
     output = "<html><body>test</body></html>"
 
     return output
@@ -20,22 +35,24 @@ def mainPage():
 
 @app.route('/restaurant/<int:restaurant_id>')
 def restaurantMenu(restaurant_id):
-    restaurant = CRUD_Operator.get_restaurant(id=restaurant_id)
-    menu_items = CRUD_Operator.get_all_items(restaurant_id=restaurant_id)
+    restaurant = Restaurant.query.filter_by(id=restaurant_id).one()
+    menu_items = MenuItem.query.filter_by(restaurant_id=restaurant_id).all()
     return render_template("menu.html", restaurant=restaurant, menu_items=menu_items)
 
 
 @app.route('/restaurant/<int:restaurant_id>/item/new_item', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
-    restaurant = CRUD_Operator.get_restaurant(id=restaurant_id)
+    restaurant = Restaurant.query.filter_by(id=restaurant_id).one()
 
     if request.method == "POST":
-        CRUD_Operator.add_menu_item(name=request.form['name'],
-                                    description=request.form['name'],
-                                    price=request.form['name'],
-                                    course=request.form['name'],
-                                    restaurant_id=restaurant.id,
-                                    add=False)
+        newItem = MenuItem(name=request.form['name'],
+                                    description=request.form['description'],
+                                    price=request.form['price'],
+                                    course=request.form['course'],
+                                    restaurant_id=restaurant.id)
+        print(newItem.name, newItem.description, newItem.price)
+        # db.session.add(newItem)
+        # db.session.commit()
         return redirect(url_for('restaurantMenu', restaurant=restaurant))
 
 
